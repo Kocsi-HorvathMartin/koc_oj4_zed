f6d54d0d27fb06729a7f0a03c8ca3066bf6ac722
Kocsi-Horváth Martin <kohomar@gmail.com>
2025-05-07 18:44:56 +0200

    Feliratkozás a /zed/depth/depth_registered és a /zed/confidence/confidence_map topic-okra

---
+++ koc_oj4_zed/zed_depth_filter.py
@@ -0,0 +1,36 @@
+from sensor_msgs.msg import Image
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+
+
+class DepthMinFilter(Node):
+    def __init__(self):
+        super().__init__('depth_min_filter')
+
+        self.bridge = CvBridge()
+        self.depth_image = None
+        self.conf_image = None
+
+        self.depth_sub = self.create_subscription(
+            Image,
+            '/zed/depth/depth_registered',
+            self.depth_callback,
+            10
+        )
+
+        self.conf_sub = self.create_subscription(
+            Image,
+            '/zed/confidence/confidence_map',
+            self.conf_callback,
+            10
+        )
+
+        self.get_logger().info("DepthMinFilter node started.")
